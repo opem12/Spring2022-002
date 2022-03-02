a2f8acd9d17bca071e1a57ba9f5f1f72751a02f6
opem12 <100376126+opem12@users.noreply.github.com>
2022-03-01 18:58:39 -0600

    Update TravelDestination.java

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -18,11 +18,12 @@ public class TravelDestination {
 				System.out.println("My name is Noah Mesa. I wish to travel to Ohio one day !");
 				System.out.println("My name is John Oranday. I wish to travel to Japan one day !");
 				System.out.println("My name is Timothy Armstrong. I wish to travel to Tokyo one day !");
-
+                                System.out.println("My name is Opeyemi Ayeni. I wish to travel to Japan one day!");
 				System.out.println("My name is Melwyn Cespedes. I wish to travel to Brazil one day !");
 				System.out.println("My name is Jorge Gonzalez. I wish to travel to Switzerland one day!");
                                 System.out.println("My name is Rizwan khan.I wish to travel to France one day !");
 				System.out.println("My name is Ernesto Palacios. I wish to travel to Venice one day!");
+		                
 	}
 
 }
